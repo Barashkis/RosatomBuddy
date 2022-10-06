2042bd3729c8963b37864baaf18b5c205dcb17eb
Andrew <dushezbaranov@gmail.com>
2022-10-06 21:51:34 +0300

    Initial project
    Added all necessary files and folders

---
+++ app.py
@@ -0,0 +1,11 @@
+from aiogram import executor
+
+from loader import dp
+
+
+async def on_startup(dispatcher):
+    pass
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
---
+++ utils/set_bot_commands.py
@@ -0,0 +1,5 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    pass
