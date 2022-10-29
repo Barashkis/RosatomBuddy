76630b2372dbf2e18c87ea7f9e2bbd0026f894d8
Andrew <dushezbaranov@gmail.com>
2022-10-29 12:19:44 +0300

    Updated logger timezone

--- logger.py
+++ logger.py
@@ -1,13 +1,35 @@
 import logging
-import datetime as dt
+import datetime
 import os
 
 from config import logs_dir, timezone
 
+
+class Formatter(logging.Formatter):
+    @staticmethod
+    def converter(timestamp):
+        dt = datetime.datetime.fromtimestamp(timestamp)
+
+        return dt
+
+    def formatTime(self, record, datefmt=None):
+        dt = self.converter(record.created).astimezone(timezone)
+
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            try:
+                s = dt.isoformat(timespec='milliseconds')
+            except TypeError:
+                s = dt.isoformat()
+
+        return s
+
+
 if not os.path.exists("logs"):
     os.mkdir("logs")
 
-today = dt.datetime.today().astimezone(timezone)
+today = datetime.datetime.today().astimezone(timezone)
 filename = f'{today.day:02d}-{today.month:02d}-{today.year}.log'
 
 logger = logging.getLogger(__name__)
@@ -20,7 +42,7 @@ file_handler.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s: %(filename)s: %(levelname)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+formatter = Formatter('%(asctime)s: %(filename)s: %(levelname)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
 
